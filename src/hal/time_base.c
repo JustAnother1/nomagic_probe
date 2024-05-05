cf5b2aab7bb6df6b4e8c9286991feccdab67a5a4
Lars Pötter <kontakt@ing-poetter.de>
2024-05-05 02:42:58 +0200

    added timeout service.

--- src/hal/time_base.c
+++ src/hal/time_base.c
@@ -12,8 +12,16 @@
  * with this program; if not, see <http://www.gnu.org/licenses/>
  *
  */
+
+#include <stddef.h>
+
 #include "hal/hw/PPB.h"
 #include "hal/time_base.h"
+#include "time.h"
+
+// comparing against < 0xffffffff is always true
+// -> we want to avoid 0xffffffff as end time of timeout
+#define MAX_SAFE_COUNT        0xfffffff0
 
 volatile uint32_t ms_since_boot;
 
@@ -48,3 +56,65 @@ uint32_t time_get_ms(void)
     return ms_since_boot;
 }
 
+void start_timeout(timeout_typ* to, const uint32_t time_ms)
+{
+    if(NULL != to)
+    {
+        uint32_t start_time = time_get_ms();
+        to->timeout_time = start_time + time_ms;
+        if(to->timeout_time > MAX_SAFE_COUNT)
+        {
+            // an end time of 0xffffffff would not work as all values are always < 0xffffffff
+            uint32_t remainder = 100 - (MAX_SAFE_COUNT - start_time);
+            to->timeout_time = 2 + remainder;
+            to->wait_for_wrap_around = true;
+        }
+        else if(to->timeout_time < start_time)
+        {
+            // wrap around
+            to->wait_for_wrap_around = true;
+        }
+        else
+        {
+            to->wait_for_wrap_around = false;
+        }
+    }
+    // else there is nothing we can do,...
+}
+
+bool timeout_expired(timeout_typ* to)
+{
+    if(NULL != to)
+    {
+        // check timeout
+        uint32_t cur_time = time_get_ms();
+        if(true == to->wait_for_wrap_around)
+        {
+            if(10 > cur_time)
+            {
+                // wrap around happened
+                to->wait_for_wrap_around = false;
+            }
+            // else continue waiting
+            return false;
+        }
+        else
+        {
+            if(cur_time > to->timeout_time)
+            {
+                // Timeout !!!
+                return true;
+            }
+            else
+            {
+                // not a timeout, yet.
+                return false;
+            }
+        }
+    }
+    else
+    {
+        // if we do not get data then that timeout expired!
+        return true;
+    }
+}
--- src/probe_api/time.h
+++ src/probe_api/time.h
@@ -15,8 +15,18 @@
 #ifndef PROBE_API_TIME_H_
 #define PROBE_API_TIME_H_
 
+#include <stdint.h>
+#include<stdbool.h>
+
+typedef struct{
+    uint32_t timeout_time;
+    bool wait_for_wrap_around;
+} timeout_typ;
 
 uint32_t time_get_ms(void);
+
+void start_timeout(timeout_typ* to, const uint32_t time_ms);
+bool timeout_expired(timeout_typ* to);
 
 
 #endif /* PROBE_API_TIME_H_ */
