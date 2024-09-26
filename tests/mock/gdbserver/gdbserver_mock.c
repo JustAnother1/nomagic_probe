d844d542a290794d058710cef01a3a99d0b29e32
Lars Pötter <lars@ing-poetter.de>
2024-09-26 02:51:40 +0200

    added tests.

--- tests/mock/gdbserver/gdbserver_mock.c
+++ tests/mock/gdbserver/gdbserver_mock.c
@@ -16,19 +16,56 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define MAX_REPLIES           10
+#define MAX_REPLY_LENGTH      2000
+
+static bool busy;
+static uint32_t reply_idx;
+static uint32_t reply_pos;
+static uint8_t reply_buffer[MAX_REPLIES][MAX_REPLY_LENGTH];
+
+void mock_gdbserver_reset_replies(void)
+{
+    reply_idx = 0;
+    reply_pos = 0;
+    memset(reply_buffer, 0, sizeof(reply_buffer));
+}
+
+uint32_t mock_gdbserver_get_num_send_replies(void)
+{
+    return reply_idx;
+}
+
+char* mock_gdbserver_get_reply(uint32_t idx)
+{
+    return &reply_buffer[idx][0];
+}
+
 void reply_packet_prepare(void)
 {
-
+    reply_buffer[reply_idx][0] = '(';
+    reply_pos = 1;
 }
 
 void reply_packet_add(char* data)
 {
-    (void) data;
+    while(0 != *data)
+    {
+        reply_buffer[reply_idx][reply_pos] = *data;
+        reply_pos++;
+        data++;
+    }
 }
 void reply_packet_add_max(char* data, uint32_t length)
 {
-    (void) data;
-    (void) length;
+    uint32_t i = 0;
+    while((0 != *data) && (i < length))
+    {
+        reply_buffer[reply_idx][reply_pos] = *data;
+        reply_pos++;
+        data++;
+        i++;
+    }
 }
 
 void reply_packet_add_hex(uint32_t data, uint32_t digits)
@@ -39,37 +76,42 @@ void reply_packet_add_hex(uint32_t data, uint32_t digits)
 
 void reply_packet_send(void)
 {
-
+    reply_buffer[reply_idx][reply_pos] = ')';
+    reply_idx++;
 }
 
 void send_error_packet(void)
 {
-
+    reply_buffer[reply_idx][0] = '-';
+    reply_idx++;
 }
 
 void send_ack_packet(void)
 {
-
+    reply_buffer[reply_idx][0] = '+';
+    reply_idx++;
 }
 
 void send_unknown_command_reply(void)
 {
-
+    reply_buffer[reply_idx][0] = '(';
+    reply_buffer[reply_idx][0] = ')';
+    reply_idx++;
 }
 
 void gdb_is_now_busy(void)
 {
-
+    busy = true;
 }
 
 void gdb_is_not_busy_anymore(void)
 {
-
+    busy = false;
 }
 
 bool is_gdb_busy(void)
 {
-    return false;
+    return busy;
 }
 
 void send_part(char* part, uint32_t size, uint32_t offset, uint32_t length)
