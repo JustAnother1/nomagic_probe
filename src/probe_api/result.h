f2a8b6663587f2c1307cd4e601e7756545e2758f
Lars Pötter <lars@ing-poetter.de>
2024-11-01 13:30:36 +0100

    added new error codes.

--- src/probe_api/result.h
+++ src/probe_api/result.h
@@ -20,16 +20,17 @@
 
 #define RESULT_OK                   0
 // Errors:                    int32_t  / uint32_t
-#define ERR_QUEUE_FULL_TRY_AGAIN   -1 // 0xffffffff
-#define ERR_WRONG_STATE            -2 // 0xfffffffe
-#define ERR_NOT_CONNECTED          -3 // 0xfffffffd
-#define ERR_NOT_COMPLETED          -4 // 0xfffffffc
-#define ERR_INVALID_TRANSACTION_ID -5 // 0xfffffffb
-#define ERR_TIMEOUT                -6 // 0xfffffffa
-#define ERR_TARGET_ERROR           -7 // 0xfffffff9
-#define ERR_INVALID_QUEUE          -8 // 0xfffffff8
-#define ERR_WRONG_VALUE            -9 // 0xfffffff7
-
+#define ERR_QUEUE_FULL_TRY_AGAIN    -1 // 0xffffffff
+#define ERR_WRONG_STATE             -2 // 0xfffffffe
+#define ERR_NOT_CONNECTED           -3 // 0xfffffffd
+#define ERR_NOT_COMPLETED           -4 // 0xfffffffc
+#define ERR_INVALID_TRANSACTION_ID  -5 // 0xfffffffb
+#define ERR_TIMEOUT                 -6 // 0xfffffffa
+#define ERR_TARGET_ERROR            -7 // 0xfffffff9
+#define ERR_INVALID_QUEUE           -8 // 0xfffffff8
+#define ERR_WRONG_VALUE             -9 // 0xfffffff7
+#define ERR_TOO_LONG               -10 // 0xfffffff6
+#define ERR_INVALID_PARAMETER      -11 // 0xfffffff5
 
 typedef int32_t Result;
 
